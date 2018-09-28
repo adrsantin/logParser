ce2f3b68fbf710158dd8480992c195ab9e7ae3f9
Adriano Santin <adr.santin@gmail.com>
2018-09-28 18:16:21 -0300

    Comentários adicionados.

--- readLog.py
+++ readLog.py
@@ -5,6 +5,7 @@ Created on Tue Sep 25 21:31:02 2018
 @author: adriano.santin
 """
 
+# operator auxilia na ordenacao das listas
 import operator
 
 logfile = open ('log.txt', 'r') 
@@ -12,7 +13,12 @@ url = {}
 rescode = {}
 
 for line in logfile:
+    # Pega somente as linhas do log que interessam: 
+    # no formato level=info response_body="" request_to"<url>" response_headers=  response_status="<code>"
     if line.startswith('level=info'):
+        
+        # Quebra as linhas em arrays e pega as informacoes necessarias: a url e o code
+        # Em seguida os guarda em um dictonary que conta o número de ocorrencias de cada url e code
         sp = line.split('"')
         if sp[3] in url:
             url[sp[3]] += 1
@@ -24,12 +30,13 @@ for line in logfile:
         else:
             rescode[sp[5]] = 1
             
+# Ordena as urls por quantidade de ocorrencias de forma decrescente e ordena os codes pelos codigos de forma crescente
 sorted_url = sorted(url.items(), key=operator.itemgetter(1), reverse=True)
 sorted_res = sorted(rescode.items(), key=operator.itemgetter(0))
+
+# Imprime os tres maiores urls em quantidade e a tabela com os codes e quantidades 
 for i in range(3):
     print (sorted_url[i][0], '-', sorted_url[i][1])
 print('\n')
 for r in sorted_res:
     print (r[0], '-', r[1])
-    
-input()
